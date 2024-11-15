fa20835728c2c94ec3b292f7e121a437d9f76551
Rohan K <rohan.kadolkar@volkswagen.co.in>
2024-11-15 19:00:22 +0530

    Adding fonts and buttons to admin scene

--- Nisha-Bot-UnityProject/NishaBotUI/Assets/Scripts/MainSceneUI.cs
+++ Nisha-Bot-UnityProject/NishaBotUI/Assets/Scripts/MainSceneUI.cs
@@ -8,17 +8,41 @@ public class MainSceneUI : MonoBehaviour
 {
     public UIDocument MainSceneUIDocument;
     private VisualElement _root;
-
     private Button AdminLoadBtn, GuestLoginBtn;
+
+    private VisualElement sideBtnPanel;
     // Start is called before the first frame update
     void Start()
     {
         _root = MainSceneUIDocument.rootVisualElement;
         AdminLoadBtn=  _root.Q<Button>("AdminLoadBtn");
         GuestLoginBtn = _root.Q<Button>("UserLoadBtn");
+
+        sideBtnPanel = _root.Q<VisualElement>("SideBtnPanel");
         
         AdminLoadBtn.RegisterCallback<ClickEvent>(LoadAdminScene);
         GuestLoginBtn.RegisterCallback<ClickEvent>(LoadAUserScene);
+
+        //CreateSideBtns();
+    }
+
+    /*void CreateSideBtns()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            var btn = new Button()
+            {
+                text = "Button "+i
+            };
+            btn.RegisterCallback<ClickEvent>(SideBtnClick);
+        
+            sideBtnPanel.Add(btn);
+        }
+    }*/
+
+    private void SideBtnClick(ClickEvent evt)
+    {
+        
     }
 
     private void LoadAdminScene(ClickEvent evt)
